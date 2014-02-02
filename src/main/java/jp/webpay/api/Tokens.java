8daf24bc79f65c05e663fab7aeb55eef2fcae452
tomykaira <tomykaira@gmail.com>
2014-02-02 18:26:55 +0900

    Add UUID parameter to Tokens

--- src/main/java/jp/webpay/api/Tokens.java
+++ src/main/java/jp/webpay/api/Tokens.java
@@ -4,13 +4,22 @@ import jp.webpay.model.Token;
 import jp.webpay.request.CardRequest;
 import lombok.NonNull;
 
+import javax.ws.rs.core.Form;
+
 public class Tokens extends Accessor {
     Tokens(WebPayClient client) {
         super(client);
     }
 
     public Token create(@NonNull CardRequest request) {
-        return Token.fromJsonResponse(client, client.post("/tokens", request.toForm()));
+        return create(request, null);
+    }
+
+    public Token create(@NonNull CardRequest request, String uuid) {
+        Form form = request.toForm();
+        if (uuid != null && !uuid.isEmpty())
+            form.param("uuid", uuid);
+        return Token.fromJsonResponse(client, client.post("/tokens", form));
     }
 
     public Token retrieve(@NonNull String id) {
--- src/test/java/jp/webpay/api/TokensTest.java
+++ src/test/java/jp/webpay/api/TokensTest.java
@@ -4,6 +4,8 @@ import jp.webpay.model.Token;
 import jp.webpay.request.CardRequest;
 import org.junit.Test;
 
+import java.util.UUID;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.containing;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static org.hamcrest.CoreMatchers.is;
@@ -23,6 +25,26 @@ public class TokensTest extends ApiTestFixture {
                 .name("YUUKO SHIONJI");
 
         Token token = client.tokens.create(card);
+
+        assertThat(token.getUsed(), is(false));
+        assertThat(token.getCard().getName(), is("YUUKO SHIONJI"));
+    }
+
+    @Test
+    public void testCreateTokenWithUUIDArgument() throws Exception {
+        String uuid = UUID.randomUUID().toString();
+        stubFor(post("/v1/tokens")
+                .withRequestBody(containing("card%5Bname%5D=YUUKO+SHIONJI"))
+                .withRequestBody(containing("uuid=" + uuid))
+                .willReturn(response("tokens/create")));
+        CardRequest card = new CardRequest()
+                .number("4242-4242-4242-4242")
+                .expMonth(12)
+                .expYear(2015)
+                .cvc(123)
+                .name("YUUKO SHIONJI");
+
+        Token token = client.tokens.create(card, uuid);
 
         assertThat(token.getUsed(), is(false));
         assertThat(token.getCard().getName(), is("YUUKO SHIONJI"));
